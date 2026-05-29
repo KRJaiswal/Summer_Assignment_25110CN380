38fec46c7be7af5393858fdc74abbd9cf93b8927
KRJaiswal <kj2038489@gmail.com>
2026-05-29 14:39:58 -0400

    Add files via upload

--- Day_2/Program1.cpp
+++ Day_2/Program1.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+int main()
+{
+   int num1,i,sum=0;
+   std::cout<<"Enter the number";
+   std::cin>>num1;
+   while (num1!=0)
+   {
+      i=num1%10;
+      sum=sum+i;
+      num1=num1/10;
 
+   }
+   std::cout<<"the sum of digit is:"<<sum;
+   return 0;
+   
+}
